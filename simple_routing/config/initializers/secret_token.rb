2d5d56de41b6612ce43d5e3e322213e920cf12e8
mettledrum <mettledrum@mettledrum-VirtualBox.(none)>
2014-08-11 11:48:13 -0600

    started simple routing blog site

---
+++ simple_routing/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SimpleRouting::Application.config.secret_key_base = '88ed44955baa06775d5e90ad01fa50131a03e63d388e430a09cb0cbd261ed93b9c40b2bd6e97faa8c825bef04de44330516df2775aa8b87ab89892bc8f006c60'
---
+++ simple_routing/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SimpleRouting::Application.config.session_store :cookie_store, key: '_simple_routing_session'
